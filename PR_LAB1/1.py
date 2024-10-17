4f6ae77e57b0e5c3707c4099ea6fb92c25093966
Lucian-Lu <lupanlucian4@gmail.com>
2024-10-17 14:46:28 +0300

    Add map, filter, reduce functions for price range filtering in euro

--- PR_LAB1/1.py
+++ PR_LAB1/1.py
@@ -1,5 +1,51 @@
 import requests
 from bs4 import BeautifulSoup
+import functools
+from datetime import datetime
+
+def get_eur_price(price_currency_pair):
+    numeric = '0123456789'
+
+    price, currency = price_currency_pair
+    price_value = price.get_text()
+    currency_value = currency.get_text()
+
+    # Error handling to ensure that the currency value is an int
+    # and that the currency type is only euro.
+    price_value = ''.join([ch for ch in price_value if ch in numeric])
+    currency_value = ''.join([ch for ch in currency_value if ch in '€'])
+
+    try:
+        price_value = int(price_value)
+    except ValueError:
+        price_value = "Error"
+    
+    if currency_value == '€':
+        return price_value
+    
+
+def add_product_price_range(products):
+    for product in products:
+        # Adding price ranges for the products
+        if isinstance(product['price'], int):
+            if product['price'] < 100:
+                product['price-range'] = 1
+            elif product['price'] < 500:
+                product['price-range'] = 2
+            elif product['price'] < 2500:
+                product['price-range'] = 3
+            elif product['price'] < 10000:
+                product['price-range'] = 4
+            else:
+                product['price-range'] = 5
+        else:
+            product['price-range'] = 0
+
+    return products
+
+def is_in_price_range(product, price_range_id):
+    return product.get('price-range') == price_range_id
+
 
 # Second task, getting the html page using a get request
 url = "https://999.md/ro/87872146"
@@ -17,11 +63,13 @@ f.write(soup.prettify())
 f.close()
 
 # Fourth task, scrapping links from the URL
-f = open("scrapped_data", "w")
+f = open("scrapped_data.txt", "w")
+
 seen_links = set()
 product_names = set()
 product_links = soup.find_all('a', {"class": "js-item-ad"})
 link_dictionary = {f'{soup.title.string}': 'https://999.md/ro/87872146'}
+
 for product_link in product_links:
     product_name = product_link.get_text(strip=True)
     link = product_link.get('href')
@@ -39,51 +87,90 @@ for product_link in product_links:
 # Writing the data to the file
 for name, link in zip(product_names, seen_links):
     link_dictionary.update({name: link})
-    # f.write(f"Product name: {name}, Product link: {link}\n")
 
 # Third task, using a html parser to get the price of a product
+temp_dict_list = []
 for i, (name, link) in enumerate(link_dictionary.items()):
+
+    temp_dict = {}
+    temp_dict["product_listing"] = i
+    temp_dict["url"] = url
+    temp_dict["title"] = name
+
     f.write("Product listing #" + str(i) + '\n')
     f.write("Link = " + link + '\n')
     f.write("Product name = " + name + '\n')
+
     response = requests.get(link)
     if response.status_code == 200:
         print("GET request was successful.")
     else:
         print(f"Error occurred while processing the request = {response.status_code}")
+    
     html_response = response.text
     soup = BeautifulSoup(html_response, 'html.parser')
     price_data = soup.find('ul', {"class": ["adPage__content__price-feature__prices"]})
+
     if price_data:
         prices = price_data.find_all('span', {"class": ["adPage__content__price-feature__prices__price__value"]})
         currencies = price_data.find_all('span', {"class": ["adPage__content__price-feature__prices__price__currency"]})
-        numeric = '0123456789'
-        standard_currency = ['€', '$', 'lei']
-        for price, currency in zip(prices, currencies):
-            price_value = price.get_text()
-            currency_value = currency.get_text()
-            # Error handling to ensure that the currency value is an int
-            # and that the currency type is only euro, dollars & lei.
-            for ch in price_value:
-                if ch not in numeric:
-                    price_value = price_value.replace(ch, '')
-            try:
-                price_value = int(price_value)
-            except:
-                price_value = "Error"
-            if currency not in standard_currency:
-                currency = "Error"
-            f.write(f"Price: {price_value}, Currency: {currency_value}\n")
+
+        price_currency_pair = zip(prices, currencies)
+        eur_prices = map(get_eur_price, price_currency_pair)
+
+        flag = 0
+        for eur_price in eur_prices:
+            if isinstance(eur_price, int):
+                flag = 1
+                f.write(f"Price: {eur_price}, Currency: €\n")
+                temp_dict["price"] = eur_price
+                temp_dict["currency"] = '€'
+        if flag == 0:
+            f.write(f"Price: Negotiable|N/A\n")
+            temp_dict["price"] = "Negotiable|N/A"
+
     else:
         f.write(f"Price: Negotiable|N/A\n")
+        temp_dict["price"] = "Negotiable|N/A"
+
     # Getting the location data
     location_data = soup.find('span', {"class": ["adPage__aside__address-feature__text"]})
     try:
         location_data = location_data.get_text(strip=True)
         if location_data:
             f.write(f"Location: {location_data}\n")
+            temp_dict["location"] = location_data
     except:
         f.write(f"Location: N/A\n")
+        temp_dict["location"] = "N/A"
+    temp_dict_list.append(temp_dict)
+
+# Sixth task, use of map, filter & reduce functions
+
+temp_dict_list = add_product_price_range(temp_dict_list)
+price_range_id = input("Select the price-range products you want to view (0-5): ")
+try:
+    price_range_id = int(price_range_id)
+    if price_range_id >= 0 and price_range_id <= 5:
+        filtered_products = list(filter(lambda product: is_in_price_range(product, price_range_id), temp_dict_list))
+        # print(filtered_products)
+        filtered_products_sum = functools.reduce(
+            lambda current, product: current + product['price'] if isinstance(product['price'], (int)) else current,
+            filtered_products,
+            0
+        )
+
+        filtered_products_results = {
+            "filtered_products": filtered_products,
+            "filtered_products_sum": filtered_products_sum,
+            "timestamp": datetime.now().isoformat()
+        }
+        # print('Sum of the products in price range[' + str(price_range_id) + '] = ' + str(filtered_products_sum))
+
+except ValueError:
+    print("Invalid data format.")
+
+print(filtered_products_results)
 
 f.close()
 
